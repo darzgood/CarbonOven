b9fc60dd153864fe6bc19f06d53f183b4b29ce5a
Darrell Good <darzgood@gmail.com>
2021-12-08 18:58:48 -0600

    Add more sensors and controls
    Here's what I have -- It needs some work to get running though

--- CarbonOven.ino
+++ CarbonOven.ino
@@ -23,13 +23,25 @@ LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
 
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
+unsigned long previousMillis = 0;        // will store last time LCD was updated
+unsigned long endTime = 0;               // will store the time when the oven should cut off (in Millis)
+const long interval = 10000;           // interval at which to update
 
+// Mode keeps track of whether the system is active or in standby
+String currentMode = "STANDBY";
 
 // constants won't change. Used here to set a pin number:
-const int heatpin = 3;
+const int powerpin = 2;
 
-const long interval = 10000;           // interval at which to blink (milliseconds)
+const int heatpin = 3;
+const int motorpin = 4;
+
+const int Rled = 9;
+const int Gled = 10;
+const int Bled = 11;
+
+
+
 
 
 void setup(void)
@@ -39,7 +51,14 @@ void setup(void)
   // Start up the library
   sensors.begin();
 
+  pinMode(powerpin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(powerpin), switchmode, FALLING);
+
   pinMode(heatpin, OUTPUT);
+  pinMode(motorpin, OUTPUT);
+  pinMode(Rled, OUTPUT);
+  pinMode(Gled, OUTPUT);
+  pinMode(Bled, OUTPUT);
 
   Serial.print("Interval: ");
   Serial.print(interval/1000);
@@ -86,6 +105,7 @@ void loop(void) {
           Serial.print("Sensor ");
           Serial.print(i);
           Serial.println(" reads negative temperatures and is likely malfunctioning.");
+          lcd.print(
 
         }
     }
@@ -124,4 +144,44 @@ void loop(void) {
       digitalWrite(heatpin, LOW); 
     }
   }
+}
+
+void switchmode() {
+  if (currentMode == "STANDBY"){
+    enterActive();
+    currentMode == "ACTIVE";
+  } else {
+    enterStandby();
+    currentMode == "STANDBY"
+  }
+}
+
+void enterActive(){
+  resetTimer();                // Reset when the system turns off
+  digitalWrite(motorpin, LOW); // Turn rotation On
+  RGB_color(100, 100, 0);      // Show Yellow (heating) Status
+}
+
+void enterStandby(){
+  digitalWrite(motorpin, HIGH);// Turn rotation off
+  digitalWrite(heatpin, HIGH); // Turn heat off
+  RGB_color(0, 0, 150);        // Show Blue (standby) status
+}
+
+void resetTimer(){
+  // At the bottom end of the potentiometer, the timer is turned off:
+  int timeVal = analogRead(0);
+  if (timeVal < 60) {
+    endTime = 0;
+  } else {
+    unsigned long heat_time_min = map(timeVal, 0, 1023, 0, 1440); // Allowable heating time is 24 H
+    endTime = millis() + heat_time_min*60*1000;
+  }
+}
+
+void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
+{
+  analogWrite(Rled, red_light_value);
+  analogWrite(Gled, green_light_value);
+  analogWrite(Bled, blue_light_value);
 }
